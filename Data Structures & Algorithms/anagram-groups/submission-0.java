6276c23550362549e039e92e83c05b62f9081adf
Shubham Kumar <shubham908@gmail.com>
2026-06-24 23:11:10 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        var result = new HashMap<String, List<String>>();
+        for (String s: strs) {
+            int[] count = new int[26];
+            for (char c: s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            result.putIfAbsent(key, new ArrayList<>());
+            result.get(key).add(s);
+        }
+
+        return new ArrayList<>(result.values());
+    }
+}
